7c9cf0352842730a7d1d2db5f36f5e069bce8632
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2009-07-15 22:55:58 +0200

    First graphics.

--- program.c
+++ program.c
@@ -15,6 +15,27 @@ static void render_scene() {
 
 	print_solitaire_info(g_solitaire);
 
+	glPushAttrib(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	int pile_index;
+	int pile_count = g_solitaire->get_pile_count(g_solitaire);
+	for(pile_index=0;pile_index<pile_count;++pile_index) {
+		pile* pile = g_solitaire->get_pile(g_solitaire, pile_index);
+
+		int card_index;
+		for(card_index=0;card_index<pile->card_count;++card_index) {
+			glBegin(GL_QUADS);
+			glVertex2f(-10.0f, 30.0f);
+			glVertex2f(10.0f, 30.0f);
+			glVertex2f(10.0f, -30.0f);
+			glVertex2f(-10.0f, -30.0f);
+
+			glEnd();
+		}
+	}
+	glPopAttrib();
+
 	glutSwapBuffers();
 }
 
@@ -23,6 +44,20 @@ static void setup_render_context() {
 }
 
 static void window_size_change(int width, int height) {
+	GLfloat aspect;
+	if(height == 0) {
+		height = 1;
+	}
+
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	aspect = (float)width/(float)height;
+	gluPerspective(45.0, aspect, 1.0, 500.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, -250.0f);
 }
 
 
