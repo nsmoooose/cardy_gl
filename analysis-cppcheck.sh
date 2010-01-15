4e6a8bfdb96c45560e0b99e3c09182eed8f65ba3
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2010-01-15 08:57:14 +0100

    Set the title of the cppcheck report created.

--- analysis-cppcheck.sh
+++ analysis-cppcheck.sh
@@ -2,4 +2,4 @@
 [ -d analysis-cppcheck ] && rm analysis-cppcheck -rf
 mkdir analysis-cppcheck
 cppcheck . --enable=all --xml 2> analysis-cppcheck/errors.xml
-cppcheck-htmlreport --file analysis-cppcheck/errors.xml --report-dir=analysis-cppcheck --source-dir=.
+cppcheck-htmlreport --file analysis-cppcheck/errors.xml --report-dir=analysis-cppcheck --source-dir=. --title cardy_gl
