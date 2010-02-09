ebf4c90a89b9cfa889f8ac4a76149e662a5a1633
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2010-02-09 16:07:49 +0100

    Creating thread for server.

--- server/program.c
+++ server/program.c
@@ -1,6 +1,8 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -11,39 +13,50 @@
 /* TODO: Create thread pool for client requests */
 /* TODO: Take thread from pool and do send/receive */
 
+void *client_handler(void *arg) {
+	printf("A thread created.\n");
+
+	return 0;
+}
+
 int main(int argc, char* argv[]) {
 	struct sockaddr_in addr;
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
 	if(fd == -1) {
 		fprintf(stderr, "failed to create socket.\n");
-		return -1;
+		return 1;
 	}
 
 	memset(&addr, 0, sizeof(struct sockaddr_in));
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(6666);
 	if(inet_pton(AF_INET, "0.0.0.0", &addr.sin_addr) != 1) {
-		fprintf(stderr, "failed to parse address.\n");
-		return -1;
+		perror("failed to parse address.\n");
+		return 1;
 	}
 	if(bind(fd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) == -1) {
-		fprintf(stderr, "failed to bind socket.\n");
-		return -1;
+		perror("failed to bind socket.\n");
+		return 1;
 	}
 
 	if(listen(fd, 3) == -1) {
-		fprintf(stderr, "failed to listen on incoming connections.\n");
-		return -1;
+		perror("failed to listen on incoming connections.\n");
+		return 1;
 	}
 
 	for(;;) {
 		int sock_fd = accept(fd, 0, 0);
 		if(sock_fd == -1) {
-			fprintf(stderr, "failed to accept socket\n");
-			return -1;
+			perror("failed to accept socket\n");
+			return 1;
 		}
 		else {
-			fprintf(stdout, "Client connected. Time to start a thread\n");
+			pthread_t thread;
+			printf("Client connected. Time to start a thread\n");
+			if(pthread_create(&thread, NULL, client_handler, (void*)sock_fd) != 0) {
+				perror("Failed to create thread.");
+				return 1;
+			}
 		}
 	}
 
