0e5cfefaaac2f1207a2201f85776ca3097dea195
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2010-02-10 21:05:53 +0100

    Some more stubs for networking.

--- api/network/game_system_remote.c
+++ api/network/game_system_remote.c
@@ -1,6 +1,16 @@
+#include <stdlib.h>
 #include "game_system_remote.h"
+
+static void game_system_get_registry(game_system *system) {
+}
+
+static void game_system_close(game_system *system) {
+}
 
 game_system *game_system_connect(const char* server, int port,
 								 const char *username, const char *password) {
-	return 0;
+	game_system *system = calloc(1, sizeof(game_system));
+	system->get_registry = game_system_get_registry;
+	system->close = game_system_close;
+	return system;
 }
