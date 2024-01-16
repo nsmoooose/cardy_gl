ffd0104cf7661307a59be8d010224ad5edee74b2
Henrik Nilsson <nsmoooose@gmail.com>
2024-01-16 19:37:18 +0100

    Adding missing files needed for prev commit

---
+++ game/card_theme.c
@@ -0,0 +1,24 @@
+#include "api/resource.h"
+#include "game/card_theme.h"
+
+static theme *g_theme = NULL;
+
+theme *card_theme_get(void) {
+	if (!g_theme) {
+		card_theme_set("paris");
+	}
+	return g_theme;
+}
+
+void card_theme_set(const char *name) {
+	if (g_theme) {
+		theme_unload(g_theme);
+		g_theme = NULL;
+	}
+
+	char theme_dir[PATH_MAX];
+	if (resource_get_dir(theme_dir, PATH_MAX)) {
+		strncat(theme_dir, "resources/card_themes", PATH_MAX - 1);
+		g_theme = theme_load(theme_dir, name);
+	}
+}
---
+++ game/card_theme.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "api/theme.h"
+
+theme *card_theme_get(void);
+void card_theme_set(const char *name);
